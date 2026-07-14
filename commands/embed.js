3ba82175e1c97e34fdf38508615580cbcf6941fc
FJVSINIXTRO <lg40217200@gmail.com>
2026-07-14 17:52:20 -0300

    Simplify embed description in embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -8,13 +8,7 @@ module.exports = {
   async execute(interaction) {
     const embed = new EmbedBuilder()
       .setTitle("FUNDADOR XARUTO")
-      .setDescription("# FUNDADOR - XARUTO - <@1402095058299129918> 
-
-**__Nosso Fundador é atual presidente xaruto__**, *__que ficará eternizado na história da FJV o mesmo foi o responsável por comandar a torcida em diversos xbondes e pistas.__*
-
--# 06/07/26 Honrarias FJV.
-
-@everyone.")
+      .setDescription("# FUNDADOR - XARUTO - <@1402095058299129918> Nosso Fundador é atual presidente xaruto__**, *__que ficará eternizado na história da FJV o mesmo foi o responsável por comandar a torcida em diversos xbondes e pistas.")
       .setColor("Red");
 
     await interaction.reply({
