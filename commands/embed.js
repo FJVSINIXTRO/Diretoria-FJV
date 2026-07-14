d2060c04dc44635563f896f081f50dee675a2a7d
FJVSINIXTRO <lg40217200@gmail.com>
2026-07-14 17:54:40 -0300

    Update embed command description and content

--- commands/embed.js
+++ commands/embed.js
@@ -3,16 +3,29 @@ const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("embed")
-    .setDescription("Envia um embed"),
+    .setDescription("Envia o painel de honrarias da FJV"),
 
   async execute(interaction) {
+
     const embed = new EmbedBuilder()
-      .setTitle("FUNDADOR XARUTO")
-      .setDescription("# FUNDADOR - XARUTO - <@1402095058299129918> Nosso Fundador é atual presidente xaruto__**, *__que ficará eternizado na história da FJV o mesmo foi o responsável por comandar a torcida em diversos xbondes e pistas.")
-      .setColor("Red");
+      .setColor("#8B0000")
+      .setTitle("🏴 FUNDADOR - XARUTO")
+      .setDescription(`
+# FUNDADOR - XARUTO - <@1402095058299129918>
+
+Nosso fundador e atual presidente, **Xaruto**, ficará eternizado na história da FJV. O mesmo foi o responsável por comandar a torcida em diversos xbondes e pistas.
+
+-# 📅 06/07/26 • Honrarias FJV
+      `)
+      .setFooter({
+        text: "Força Jovem Vasco • Diretoria"
+      })
+      .setTimestamp();
 
     await interaction.reply({
+      content: "@everyone",
       embeds: [embed]
     });
-  }
+
+  },
 };
